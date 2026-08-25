0d59a9fca3cc276dd1d292cb75a6d03e5fcaffba
Steven Tavares <stavares88@gmail.com>
2026-08-25 15:07:19 -0600

    Initialize Orbital Ops dashboard

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Orbital Ops</h1>
+      <p>Satellite Mission Operations Dashboard</p>
+    </main>
+  );
+}
+
+export default App;
